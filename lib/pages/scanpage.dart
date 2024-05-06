76cc60c9e29e40b371b56d23c3227aede0f3046c
Sutthiphong <azballkung@Sutthiphongs-MacBook-Air.local>
2024-05-06 17:25:03 +0700

    fix scanpage but not finish

--- lib/pages/scanpage.dart
+++ lib/pages/scanpage.dart
@@ -30,10 +30,37 @@ class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Row(
+    return
+        // appBar: AppBar(
+        //   backgroundColor: Colors.red,
+        //   title: Row(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       TextButton(
+        //           onPressed: () {
+        //             setState(() {
+        //               _page = 1;
+        //             });
+        //           },
+        //           child: Text('first')),
+        //       TextButton(
+        //           onPressed: () {
+        //             setState(() {
+        //               _page = 2;
+        //             });
+        //           },
+        //           child: Text('Second')),
+        //     ],
+        //   ),
+        // ),
+
+        Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.blue,
+      child: Column(
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
@@ -52,52 +79,83 @@ class _ScanPageState extends State<ScanPage> {
                   child: Text('Second')),
             ],
           ),
-        ),
-        body: Column(
-          children: [
-            _page == 1
-                ? Container(
-                    height: 678,
-                    child: Stack(alignment: Alignment.center, children: [
-                      FutureBuilder(
-                          future: cameraValue,
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.done) {
-                              return Padding(
-                                padding: const EdgeInsets.all(40),
-                                child: SizedBox(
-                                  width: size.width,
-                                  height: size.height,
-                                  child: FittedBox(
-                                    fit: BoxFit.fitHeight,
-                                    child: SizedBox(
-                                      width: 100,
-                                      child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(2),
-                                          child:
-                                              CameraPreview(cameraController)),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else {
-                              return Center(child: CircularProgressIndicator());
-                            }
-                          }),
-                      Container(
-                        width: 350,
-                        child: Image.asset('assets/scanner/scannerScope.png'),
-                      )
-                    ]),
-                  )
-                : Container(
-                    width: size.width,
-                    height: 800,
-                    child: Center(child: Text('Mark\'s page')),
-                  )
-          ],
-        ));
+          _page == 1
+              ? Container(
+                  width: size.width * 0.9,
+                  height: size.height * 0.8,
+                  color: Colors.amber,
+                  child: Stack(alignment: Alignment.center, children: [
+                    FutureBuilder(
+                        future: cameraValue,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            return CameraPreview(cameraController);
+                          } else {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                        }),
+                    Container(
+                      width: 350,
+                      child: Image.asset('assets/scanner/scannerScope.png'),
+                    )
+                  ]),
+                )
+              : Container(
+                  width: size.width,
+                  height: 800,
+                  child: Center(child: Text('Mark\'s page')),
+                )
+        ],
+      ),
+    );
+    // body: SizedBox(
+    //     width: MediaQuery.of(context).size.width,
+    //     height: MediaQuery.of(context).size.height,
+    //     child: Column(
+    //       children: [
+    //         _page == 1
+    //             ? Container(
+    //                 color: Colors.amber,
+    //                 height: 678,
+    //                 child: Stack(alignment: Alignment.center, children: [
+    //                   FutureBuilder(
+    //                       future: cameraValue,
+    //                       builder: (context, snapshot) {
+    //                         if (snapshot.connectionState ==
+    //                             ConnectionState.done) {
+    //                           return SizedBox(
+    //                             width: size.width,
+    //                             height: size.height,
+    //                             child: FittedBox(
+    //                               fit: BoxFit.fitHeight,
+    //                               child: SizedBox(
+    //                                 width: 100,
+    //                                 child: ClipRRect(
+    //                                     borderRadius:
+    //                                         BorderRadius.circular(2),
+    //                                     child:
+    //                                         CameraPreview(cameraController)),
+    //                               ),
+    //                             ),
+    //                           );
+    //                         } else {
+    //                           return Center(
+    //                               child: CircularProgressIndicator());
+    //                         }
+    //                       }),
+    //                   Container(
+    //                     width: 350,
+    //                     child: Image.asset('assets/scanner/scannerScope.png'),
+    //                   )
+    //                 ]),
+    //               )
+    //             : Container(
+    //                 width: size.width,
+    //                 height: 800,
+    //                 child: Center(child: Text('Mark\'s page')),
+    //               )
+    //       ],
+    //     )),
   }
 }
